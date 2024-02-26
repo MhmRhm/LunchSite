633a4e1f042b376b48fad902e8a4da3902c72996
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2024-02-26 04:55:20 +0000

    load .env file each time run send daily mail

--- menus/tasks.py
+++ menus/tasks.py
@@ -4,15 +4,15 @@ from .models import Employee
 from django.core.mail import send_mail
 from dotenv import load_dotenv
 
-load_dotenv()
-
-
 def send_daily_email():
+    load_dotenv()
+    
     subject = "Daily Lunch Remainder"
     message = "Check the status at: 192.168.1.126:8172"
     employees = Employee.objects.all()
     recipient_list = [
         employee.user.email for employee in employees if len(employee.user.email) != 0
     ]
-    print(recipient_list)
+    
+    print(os.getenv("EMAIL_HOST_USER"), recipient_list)
     send_mail(subject, message, os.getenv("EMAIL_HOST_USER"), recipient_list)
