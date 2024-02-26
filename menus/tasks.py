1bb8e7da894d940dbe985c2fa127b137559eb8ca
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2024-02-26 04:30:49 +0000

    remove empty emails from the list

--- menus/tasks.py
+++ menus/tasks.py
@@ -6,10 +6,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def send_daily_email():
     subject = "Daily Lunch Remainder"
     message = "Check the status at: 192.168.1.126:8172"
     employees = Employee.objects.all()
-    recipient_list = [employee.user.email for employee in employees]
+    recipient_list = [
+        employee.user.email for employee in employees if len(employee.user.email) != 0
+    ]
     print(recipient_list)
     send_mail(subject, message, os.getenv("EMAIL_HOST_USER"), recipient_list)
